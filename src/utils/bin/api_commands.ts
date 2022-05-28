d574bd2c3771da39cbbee9040d26468c43f89916
Harry Hopkinson <63599884+Harry-Hopkinson@users.noreply.github.com>
2022-05-28 19:04:41 +0000

    Change ASCII art.

--- src/utils/bin/api_commands.ts
+++ src/utils/bin/api_commands.ts
@@ -22,7 +22,7 @@ export const quote = async (args: string[]): Promise<string> => {
 export const weather = async (args: string[]): Promise<string> => {
   const city = args.join('+');
   if (!city) {
-    return 'Usage: weather [city]. Example: weather casablanca';
+    return 'Usage: weather [city]. Example: weather London';
   }
   const weather = await getWeather(city);
   return weather;
