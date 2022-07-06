f6b850a8a55bc961660112f16c232f6bbb26892b
Harry Hopkinson <harryjhopkinson@gmail.com>
2022-07-06 21:31:18 +0100

    Bat Script

---
+++ install/install.bat
@@ -0,0 +1,36 @@
+
+echo "git clone 'https://github.com/Harry-Hopkinson/terminal.git'"
+
+cd terminal
+
+echo " Terminal cloned! Installing dependencies..."
+
+yarn install
+
+
+cat <<-'EOM'
+
+
+
+██╗  ██╗ █████╗ ██████╗ ██████╗ ██╗   ██╗    ██╗  ██╗ ██████╗ ██████╗ ██╗  ██╗██╗███╗   ██╗███████╗ ██████╗ ███╗   ██╗
+██║  ██║██╔══██╗██╔══██╗██╔══██╗╚██╗ ██╔╝    ██║  ██║██╔═══██╗██╔══██╗██║ ██╔╝██║████╗  ██║██╔════╝██╔═══██╗████╗  ██║
+███████║███████║██████╔╝██████╔╝ ╚████╔╝     ███████║██║   ██║██████╔╝█████╔╝ ██║██╔██╗ ██║███████╗██║   ██║██╔██╗ ██║
+██╔══██║██╔══██║██╔══██╗██╔══██╗  ╚██╔╝      ██╔══██║██║   ██║██╔═══╝ ██╔═██╗ ██║██║╚██╗██║╚════██║██║   ██║██║╚██╗██║
+██║  ██║██║  ██║██║  ██║██║  ██║   ██║       ██║  ██║╚██████╔╝██║     ██║  ██╗██║██║ ╚████║███████║╚██████╔╝██║ ╚████║
+╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝       ╚═╝  ╚═╝ ╚═════╝ ╚═╝     ╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝╚══════╝ ╚═════╝ ╚═╝  ╚═══╝                                                                                                                                                                                   
+
+                                                                                
+
+LiveTerm has been downloaded to the current directory.
+You can start developing it with:
+
+cd LiveTerm && yarn dev
+
+This will open a local port at http://localhost:3000, 
+where you can preview your website. To apply changes, edit:
+
+config.json
+
+And the changes should be applied upon saves!
+
+EOM
